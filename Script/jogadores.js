0c5422d02c7182e727f4e402bd4da573db87696a
Caue Lima <cauelima2705@gmail.com>
2024-04-14 17:27:27 -0300

    Co-authored-by: Matheus Francisco <theusjf@users.noreply.github.com>

--- Script/jogadores.js
+++ Script/jogadores.js
@@ -13,15 +13,31 @@ const jogadores = [
 const pontosJogador = document.getElementById('pontuacao')
 
 let jogadorAtual = jogadores[0]
+const vez = document.getElementById('vez')
 
 function mudarJogador() {
     if (jogadorAtual === jogadores[0]) {
         jogadorAtual = jogadores[1]
+        vez.innerText='É a vez de '+ jogadorAtual.nome + '!'
     } else {
         jogadorAtual = jogadores[0]
+        vez.innerText='É a vez de '+ jogadorAtual.nome + '!'
     }
 }
 
 function mudarPontuacaoJogador() {
-    jogadorAtual.pontos
+    jogadorAtual.pontos = jogadorAtual.pontos + pontuacao
 }
+
+function mostrarPontuacao() {
+    pontosJogador.innerHTML = 'Jogador 1: ' + jogadores[0].pontos + ' pontos.<div>Jogador 2: ' + jogadores[1].pontos + ' pontos.</div>';
+}
+
+function mudarTurno(){
+    mudarJogador()
+    reiniciar()
+}
+
+
+
+
